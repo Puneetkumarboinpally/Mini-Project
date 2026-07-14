23843a8c35e99401fbfc65a8102c27e181b5f3be
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-07-15 00:00:33 +0100

    login page started

--- day-007-React-Authentication-App/src/pages/login.jsx
+++ day-007-React-Authentication-App/src/pages/login.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import { Link } from "react-router-dom";
 const login = () => {
   return (
-    <div>login</div>
-  )
-}
+    <div>
+      <div className="flex flex-col justify-center items-center h-full">
+        <h1>Login</h1>
+        <form>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="email" placeholder="Enter your email here..." />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input type="password" placeholder="Enter your password here..." />
+          </div>
+          <button>login</button>
+          <p>
+            Dont have an account?
+            <Link to={"/signup"}>Signup</Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default login
+export default login;
