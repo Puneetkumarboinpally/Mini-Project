95b49620c3102ca14ef8e1ac408bb14fbd11c441
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-07-15 01:22:39 +0100

    login ui updated

--- day-007-React-Authentication-App/src/pages/login.jsx
+++ day-007-React-Authentication-App/src/pages/login.jsx
@@ -1,22 +1,41 @@
 import { Link } from "react-router-dom";
 const login = () => {
   return (
-    <div>
-      <div className="flex flex-col justify-center items-center h-full">
-        <h1>Login</h1>
+    <div className="h-full flex justify-center items-center">
+      <div className="w-full max-w-md p-8 border-2 bg-white/20 rounded-xl border-white/30">
+        <h1 className="text-4xl font-bold text-text mb-4">Login</h1>
         <form>
-          <div>
-            <label htmlFor="email">Email</label>
-            <input type="email" placeholder="Enter your email here..." />
+          <div className="flex flex-col mb-2">
+            <label className="text-text text-lg mb-1 font-bold" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="border p-2 rounded text-text outline-accent"
+              type="email"
+              placeholder="Enter your email here..."
+            />
           </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input type="password" placeholder="Enter your password here..." />
+          <div className="flex flex-col mb-4">
+            <label className="text-text text-lg font-bold mb-1" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="border p-2 rounded text-text outline-accent"
+              type="password"
+              placeholder="Enter your password here..."
+            />
           </div>
-          <button>login</button>
-          <p>
+          <button
+            type="button"
+            className="my-2 py-2 px-6 rounded-lg text-lg font-bold text-text 
+          bg-accent cursor-pointer active:scale-95
+          transition-all duration-200 hover:translate-y-[-4px] "
+          >
+            login
+          </button>
+          <p className="text-lg text-text font-semibold">
             Dont have an account?
-            <Link to={"/signup"}>Signup</Link>
+            <Link className="text-accent font-bold cursor-pointer underline mx-2" to={"/signup"}>Signup</Link>
           </p>
         </form>
       </div>
